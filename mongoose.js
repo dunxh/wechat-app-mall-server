1604a74efade9f7a3f51c776eddc6670aa59f264
dunxh <dunxinghong@163.com>
2019-12-13 16:11:24 +0800

    初始化

---
+++ mongoose.js
@@ -0,0 +1,4 @@
+﻿var mongoose = require('mongoose')
+mongoose.connect('mongodb://root:dunxh_mongo1@dds-bp1e94b48f451dd41916-pub.mongodb.rds.aliyuncs.com:3717/admin', { useNewUrlParser: true, useUnifiedTopology: true }) //服务器
+mongoose.Promise = global.Promise
+module.exports = mongoose
